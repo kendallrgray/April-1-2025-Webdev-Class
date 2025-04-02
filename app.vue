3948d1aa1ed23dd53474ee32157ae45384dc6629
kendallrgray <kendall.grayy@gmail.com>
2025-04-01 19:42:16 -0600

    app vue change

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="text-blue-500">
-    <section>Happy Birthday</section>
+    <section>Happy Birthday
+
+      A snake walked into a bar and the bartender said, "How'd you do that"
+    </section>
   </div>
 </template>
