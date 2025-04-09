790223745ebb47c06e16b0dfc3206419feb28944
kendallrgray <kendall.grayy@gmail.com>
2025-04-08 18:53:56 -0600

    updated index - app.vue

--- app.vue
+++ app.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="text-blue-500">
-    <section class="text-green-500/80">
+    <NuxtLayout>
+      <NuxtPage>
+        
+      </NuxtPage>
+    </NuxtLayout>
+    
       A snake walked into a bar and the bartender said, "How'd you do that"
-    </section>
+  
   </div>
 </template>
